166a6e35f574f32d2f294e12931abb9b20003df3
Ross Sihovsk <rsihovskiy@gmail.com>
2025-11-08 16:26:40 +0200

    Fix exception while trying to get app name

--- app/src/main/java/com/ross/livemedia/utils/Extensions.kt
+++ app/src/main/java/com/ross/livemedia/utils/Extensions.kt
@@ -2,4 +2,10 @@ package com.ross.livemedia.utils
 
 import android.content.pm.PackageManager
 
-fun PackageManager.getAppName(pkg: String) = getApplicationLabel(getApplicationInfo(pkg, 0))
+fun PackageManager.getAppName(pkg: String): CharSequence {
+    return try {
+        getApplicationLabel(getApplicationInfo(pkg, 0))
+    } catch (e: Exception) {
+        "Unknown App"
+    }
+}
