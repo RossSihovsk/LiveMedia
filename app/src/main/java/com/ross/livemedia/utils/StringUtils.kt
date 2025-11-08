83a5231f947727b2de9a43748d21a7f9f4b3e281
Ross Sihovsk <rsihovskiy@gmail.com>
2025-11-08 16:41:04 +0200

    Add character limitation

--- app/src/main/java/com/ross/livemedia/utils/StringUtils.kt
+++ app/src/main/java/com/ross/livemedia/utils/StringUtils.kt
@@ -3,5 +3,15 @@ package com.ross.livemedia.utils
 import com.ross.livemedia.media.MusicState
 import com.ross.livemedia.media.MusicState.Companion.EMPTY_ALBUM
 
-fun buildArtisAlbumTitle(musicState: MusicState) =
-    if (musicState.albumName == EMPTY_ALBUM) musicState.artist else "${musicState.artist} - ${musicState.albumName}"
+private const val MAX_LENGTH = 70
+
+fun buildArtisAlbumTitle(musicState: MusicState): String {
+    val baseResult =
+        if (musicState.albumName == EMPTY_ALBUM) musicState.artist else "${musicState.artist} - ${musicState.albumName}"
+
+    return if (baseResult.length > MAX_LENGTH) {
+        baseResult.substring(0, MAX_LENGTH) + "..."
+    } else {
+        baseResult
+    }
+}
